74265d68475f57329c167642290044248ae6a462
Vlastimil Kleibl <vlastimil.kleibl03@gmail.com>
2026-08-18 16:27:57 +0200

    basic API communication and project setup based on Discord example app
    https://github.com/discord/discord-example-app

---
+++ src/commands.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { capitalize, InstallGlobalCommands } from './utils.js';
+
+
+// Simple test command
+const TEST_COMMAND = {
+  name: 'test',
+  description: 'Basic command',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+};
+
+
+const ALL_COMMANDS = [TEST_COMMAND];
+
+InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
